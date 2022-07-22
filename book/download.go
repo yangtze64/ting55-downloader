e19a8b50f3806bab193f25c8fe3261011770a885
YCJ <yuchangjiang@hulianpai.com>
2022-07-22 18:17:29 +0800

    s

--- book/download.go
+++ book/download.go
@@ -2,7 +2,9 @@ package book
 
 import (
 	"fmt"
+	"github.com/cheggaaa/pb/v3"
 	"log"
+	"time"
 	"ting55-downloader/pkg/console"
 )
 
@@ -29,5 +31,12 @@ func NewDownloader(num int, mode int, dpath string) *downloader {
 
 func (d *downloader) Download(book *Book) {
 	log.Println(console.Green(fmt.Sprintf("Start Download Book,BookId:%d BookName:%s", book.Id, book.Title)))
-
+	tmpl := `{{ green "With funcs:" }} {{ bar . "[" "#" (cycle . "=>" "=>" "=>" "=>" ) "." "]" | green}} {{speed . | green }} {{percent .}} {{string . "my_green_string" | green}} {{string . "my_blue_string" | blue}}`
+	bar := pb.ProgressBarTemplate(tmpl).Start(10000)
+	for i := 0; i < 10000; i++ {
+		bar.Increment()
+		time.Sleep(time.Millisecond)
+	}
+	bar.Set(pb.SIBytesPrefix, true)
+	bar.Finish()
 }
